624fb6ed98473d6538c4b7dbebcb19808f989070
nataliesun <natsun121@gmail.com>
2019-07-24 22:48:57 -0700

    Updates

---
+++ precache-manifest.8487da3982edcd412ebbf77bb9f01d7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd61924852666cca5aee7282eb1de485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21774552c5dd71a995c2",
+    "url": "/static/css/main.838641d9.chunk.css"
+  },
+  {
+    "revision": "b56666b38f5b01e4d8d9",
+    "url": "/static/js/2.b0e7542f.chunk.js"
+  },
+  {
+    "revision": "21774552c5dd71a995c2",
+    "url": "/static/js/main.91a37516.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9622e9c7a215df86eb5b6fd1de06d01",
+    "url": "/static/media/undraw_startled.c9622e9c.svg"
+  }
+]);
